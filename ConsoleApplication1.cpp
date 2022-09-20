947208ddb51cb0c1cfa6b0560495271a902e7003
Phust552002 <89695001+Phust552002@users.noreply.github.com>
2022-09-20 09:37:07 +0700

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello Wojuhuod!\n";
+    std::cout << "Hello gsuhgsglkhkgslkjbnlksgnln!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
